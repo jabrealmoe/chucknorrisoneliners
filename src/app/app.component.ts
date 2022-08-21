8fb82477aa94c4fc6e8ac4f402358e7dffc1b86a
Jabreal Johnson <jabrealthecoder@gmail.org>
2022-08-21 16:04:51 -0400

    Refactory index page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
       imageAlt: 'nature1'
     },
     {
-      imageSrc: 'https://i.ytimg.com/vi/KvZT3etZIsw/maxresdefault.jpg',
+      imageSrc: 'https://media.istockphoto.com/photos/coach-drawing-american-football-game-playbook-picture-id1366672302?b=1&k=20&m=1366672302&s=170667a&w=0&h=icvKGPFCVclcwsj0l3D91o2MStAYTEGRLijEs2JiCwk=',
       imageAlt: 'nature2'
     },
     {
@@ -23,11 +23,15 @@ export class AppComponent {
       imageAlt: 'nature3'
     },
     {
-      imageSrc: 'https://images.pexels.com/photos/1003914/pexels-photo-1003914.jpeg',
+      imageSrc: 'https://miro.medium.com/max/1400/1*3Fxk3b8h1kvKJFe5gVf98A.png',
       imageAlt: 'nature4'
     },
     {
-      imageSrc: 'https://images.pexels.com/photos/1486861/pexels-photo-1486861.jpeg',
+      imageSrc: 'https://wac-cdn-2.atlassian.com/image/upload/f_auto,q_auto/dam/jcr:786fd7a8-9b53-4b73-af03-fb1b76992638/SMT-2480_JiraAlign_Screenshots-01-ProgramManagement.png',
+      imageAlt: 'Jira Strategy'
+    },
+    {
+      imageSrc: 'https://www.worldofagile.com/wp-content/uploads/2018/03/DevOpsToolchain.png',
       imageAlt: 'nature5'
     },
   ]
